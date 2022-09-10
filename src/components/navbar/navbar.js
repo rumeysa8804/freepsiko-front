9c74d05b76b72e5b0defbad675ec01ca06a38697
bssenoz <bsenoz@hotmail.com>
2022-09-10 14:06:12 +0300

    missing modals added

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,8 +7,22 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import logo from "../../img/logo.svg";
 import styles from "../navbar/navbar.module.css";
+import { useDisclosure } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 function NavScrollExample() {
+  const { isOpen: isAboutOpen, onOpen: onAboutOpen, onClose: onAboutClose } = useDisclosure()
+  const { isOpen: isHelpOpen, onOpen: onHelpOpen, onClose: onHelpClose } = useDisclosure()
+  
   return (
+    <>
     <Navbar className={styles.navbar_container} expand="lg" style={{position:"fixed",width:"100%",backdropFilter: "blur(2px)"}}>
       <Container className={styles.navbar_container2}>
         <Navbar.Brand href="#">
@@ -33,10 +47,10 @@ function NavScrollExample() {
             <div className={`${styles.navbar_navlink}`}>Çalışan Ol</div>
           </Link>
           <Link className={styles.navbar_link} to="/">
-            <div className={`${styles.navbar_navlink}`}>Hakkında</div>
+            <div className={`${styles.navbar_navlink}`} onClick={onAboutOpen}>Hakkında</div>
           </Link>
           <Link className={styles.navbar_link} to="/">
-            <div className={`${styles.navbar_navlink}`}>Yardım</div>
+            <div className={`${styles.navbar_navlink}`} onClick={onHelpOpen}>Yardım</div>
           </Link>
           <Link className={styles.navbar_link} to="/login">
             <button variant="" className={`${styles.navbar_button} rounded`}>
@@ -46,6 +60,41 @@ function NavScrollExample() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <Modal onClose={onAboutClose} isOpen={isAboutOpen} isCentered>
+        <ModalOverlay       bg='blackAlpha.300'
+      backdropFilter='blur(10px) ' />
+        <ModalContent  bg='blackAlpha.200' style={{border: "1px solid white", borderRadius: "15px"}}
+      backdropFilter='blur(50px)'>
+          {/* <ModalHeader style={{color: "white"}}>Why freelance</ModalHeader> */}
+          <ModalCloseButton style={{backgroundColor: "white", borderRadius: "20px"}} />
+          <ModalBody>
+            <div style={{color: "white", marginTop: "40px", fontSize: "18px", fontWeight: 700}}>
+           HAKKINDA
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <img className={styles.modal_logo} src={logo}/>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Modal onClose={onHelpClose} isOpen={isHelpOpen} isCentered>
+        <ModalOverlay       bg='blackAlpha.300'
+      backdropFilter='blur(10px) ' />
+        <ModalContent  bg='blackAlpha.200' style={{border: "1px solid white", borderRadius: "15px"}}
+      backdropFilter='blur(50px)'>
+          {/* <ModalHeader style={{color: "white"}}>Why freelance</ModalHeader> */}
+          <ModalCloseButton style={{backgroundColor: "white", borderRadius: "20px"}} />
+          <ModalBody>
+            <div style={{color: "white", marginTop: "40px", fontSize: "18px", fontWeight: 700}}>
+           YARDIM
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <img className={styles.modal_logo} src={logo}/>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
